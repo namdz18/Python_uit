7a503a0b0d88036ed1e3dedb2ab247f5e6842745
testPython <namngo102003@gmail.com>
2021-11-28 23:58:23 +0700

    add compare_two_number

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from utils.utils import add_int
+from utils.utils import *
 
-if __name__=='__main__':
+if __name__ == '__main__':
     # print(f'hello world!')
     a = 10
     b = 8
     result = add_int(a, b)
     print(f'a + b = {result}')
+    compare_two_number(a, b)
--- utils/utils.py
+++ utils/utils.py
@@ -6,3 +6,12 @@ def add_int(a, b):
 def add_float(a, b) -> float:
     result = a + b
     return result
+
+
+def compare_two_number(a: int, b: int):
+    if a == b:
+        print(f'A {a} equals B {b}')
+    elif a > b:
+        print(f'A {a} greater than B {b}')
+    else:
+        print(f'A {a} less than B {b}')
